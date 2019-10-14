c59be7f367d58ea4f6d82a99377e62f58a195c7d
Anton <a.kostychenko@doctis.ru>
2019-10-14 16:22:49 +0300

    simple connection to stun server

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using LumiSoft.Net.STUN.Client;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket
+         (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+
+            // Query STUN server
+            STUN_Result result = STUN_Client.Query("stun.sipgate.net", 3478, socket);
+            if (result.NetType != STUN_NetType.UdpBlocked)
+            {
+                // UDP blocked or !!!! bad STUN server
+            }
+            else
+            {
+                IPEndPoint publicEP = result.PublicEndPoint;
+                // Do your stuff
+            }
+            Console.ReadLine();
+        }
+    }
+}
